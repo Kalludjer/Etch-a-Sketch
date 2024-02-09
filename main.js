98d0b7976c2c0e57924d40a0a739609c86a4b5fe
Kalludjer <necicdragan3@GMAIL.COM>
2024-02-09 18:59:53 +0100

    add stayling to divs and event litener

---
+++ main.js
@@ -0,0 +1,24 @@
+const resetButton = document.querySelector("#popup");
+const container = document.querySelector(".container");
+
+const createGrid = (amtOfGrids) => {
+    for (let i = 0; i < amtOfGrids; i++){
+        const row = document.createElement("div");
+        row.classList.add("grid-row");
+
+        for (let j = 0; j < amtOfGrids; j++) {
+            const widthAndHeight = 500 / sizeOfGrid;
+            const gridBox = document.createElement("div");
+            gridBox.classList.add('grid-box');
+            gridBox.style.width = `$[widthAndHeight]px`;
+            gridBox.style.height = `$[widthAndHeight]px`;
+
+            gridBox.addEventListener("mouseenter", () => {
+                gridBox.style.backgroundColor = "black"
+            })
+            row.appendChild(gridBox);
+        }
+
+        container.appendChild(row);
+    }
+}
