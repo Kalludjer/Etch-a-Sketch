8fa777aa60f38f6b7abfbad472049b563a6f51b9
Kalludjer <necicdragan3@GMAIL.COM>
2024-02-15 02:38:01 +0100

    add game

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-const resetButton = document.querySelector("#popup");
+const getNumber = document.querySelector("#getNumber");
 const container = document.querySelector(".container");
 
-const createGrid = (amtOfGrids) => {
-    for (let i = 0; i < amtOfGrids; i++){
-        const row = document.createElement("div");
-        row.classList.add("grid-row");
+getNumber.addEventListener("click", () =>{
+  container.replaceChildren();
+  const newNumber = prompt("Enter an even number, maximum 100");
+  if (
+    isNaN(newNumber) == false &&
+    newNumber <= 100 &&
+    newNumber > 0 &&
+    newNumber % 2 == 0 
+  ){
+    createGrids (newNumber);
+  }else {
+    alert ("Error! You entered an incorrect value.");
+  }
+});
 
-        for (let j = 0; j < amtOfGrids; j++) {
-            const widthAndHeight = 500 / sizeOfGrid;
-            const gridBox = document.createElement("div");
-            gridBox.classList.add('grid-box');
-            gridBox.style.width = `$[widthAndHeight]px`;
-            gridBox.style.height = `$[widthAndHeight]px`;
+function createGrids(resolution) {
+  for(i = 0; i < resolution; i++){
+    const container = document.querySelector(".container");
+    const gridVertical = document.createElement("div");
+    gridVertical.classList.add("gridVertical");
+    container.appendChild(gridVertical);
+  }
 
-            gridBox.addEventListener("mouseenter", () => {
-                gridBox.style.backgroundColor = "black"
-            })
-            row.appendChild(gridBox);
-        }
+  const gridVerticalElements = document.querySelectorAll(".gridVertical");
 
-        container.appendChild(row);
-    }
+  gridVerticalElements.forEach((gridVerticalContainer) => {
+    Array.from({length: resolution}).forEach(() => {
+      const gridHorizontal = document.createElement("div");
+      gridHorizontal.classList.add("gridHorizontal");
+      gridVerticalContainer.appendChild(gridHorizontal);
+    })
+  })
 }
+
